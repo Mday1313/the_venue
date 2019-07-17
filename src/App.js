fdd8395b417c0804e1591fae26d83187e7526a4b
Mday1313 <melissaday076@gmail.com>
2019-07-17 11:14:13 -0400

    front end complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import "./resources/styles.css";
+import { Element } from "react-scroll";
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueInfo from './components/venueInfo';
 import Highlights from './components/highlights';
 import Pricing from './components/Pricing';
+import Location from './components/location';
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
   return (
     <div className="App" style={{ height: "1500px"}}>
       <Header />
-      <Featured />
-      <VenueInfo />
-      <Highlights/>
-      <Pricing />
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="venue_info"> 
+        <VenueInfo />
+      </Element>
+      <Element name="highlight"> 
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
+      <Footer />
     </div>
   );
 }
